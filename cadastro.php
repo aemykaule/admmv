98c2ad6c568b591229992c3d2da7e249c6166ee2
maaribam <marianahuamai1206@gmail.com>
2026-08-13 11:39:41 -0300

    11:39

--- cadastro.php
+++ cadastro.php
@@ -3,10 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/output.css">
     <title>Document</title>
 </head>
 <body>
-    lets goo
+    
 </body>
 </html>
-<link rel="stylesheet" href="css/output.css">
